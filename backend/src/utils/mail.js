8e0d29ad655f755ebde099d5aa71f444196ed69d
Devansh Garg <devanshgargmg@gmail.com>
2025-11-29 09:17:00 +0530

    Fix email SMTP configuration

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -4,10 +4,15 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 });
 
