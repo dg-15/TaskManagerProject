417dc635568934add0e6fe85af60416e168ac448
Devansh Garg <devanshgargmg@gmail.com>
2025-11-29 09:22:44 +0530

    back to gmail configuration

--- backend/src/utils/mail.js
+++ backend/src/utils/mail.js
@@ -4,15 +4,10 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
-  },
-  tls: {
-    rejectUnauthorized: false,
   },
 });
 
